a24e33a6a236935c6d7900d66d8c8feadf1b1b3b
Prabhanjan Mahapatra <mahapatra.prabhanjan@hotmail.com>
2018-06-28 20:12:47 +0530

    Base Server Project In progress

---
+++ src/Samples/Server/OidcServer/Configuration/Config.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using IdentityServer4.Models;
+
+namespace OidcServer.Configuration
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api1","My API")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId="Xamarin",
+                    ClientName="Xamarin Test Client",
+                    AllowedGrantTypes = GrantTypes.Hybrid,
+                    AllowOfflineAccess = true,
+                    AllowedScopes = {"api1"},
+                    RequirePkce = true,
+                    RequireClientSecret = false,
+                    AllowAccessTokensViaBrowser = true,
+                    RedirectUris = {"xamarinclient://identitycallback"},
+                    PostLogoutRedirectUris = {"xamarinclient://logut"}
+                }
+            };
+        }
+    }
+}
